a3f0fd5f8037bc6c47bdbc8025ad1d10bfc94b09
Patrick <pderuiter@gmail.com>
2017-09-27 13:19:51 +0200

    Added global exception handler with logging

--- ACEOMM/App.xaml.cs
+++ ACEOMM/App.xaml.cs
@@ -1,8 +1,21 @@
-﻿using System.Windows;
+using NLog;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace ACEOMM
 {
     public partial class App : Application
     {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            logger.Fatal(e.Exception);
+        }
     }
 }
